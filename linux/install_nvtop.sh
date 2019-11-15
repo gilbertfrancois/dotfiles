b8ff5c6766313f08f15ecab3225340ea06522d13
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2019-11-15 14:08:30 +0100

    Updates build script

--- linux/install_nvtop.sh
+++ linux/install_nvtop.sh
@@ -8,7 +8,7 @@ mkdir -p nvtop/build && cd nvtop/build
 
 # If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
 # try the following command instead, otherwise skip to the build with make.
-cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+cmake ..
 
 make
 sudo make install
