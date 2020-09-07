0f716cdd3bac3449ae50d7c5abd679b713cfd274
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2020-09-07 12:03:48 +0200

    Adds poetry install script.

--- python/install_poetry.sh
+++ python/install_poetry.sh
@@ -1,25 +1,24 @@
-set e
 #!/usr/bin/env bash
 
 set -e
 function update_sh {
-    case `grep -sqF ".poetry/bin" ${HOME}/$1 > /dev/null; echo $?` in 
-        0) 
-            echo "--- Adding poetry to the path."
-            echo 'export ${HOME}/.poetry/bin' >> ~/$1
-            source ${HOME}/$1
+    case `grep -sqF "poetry/bin" ${HOME}/$1 > /dev/null; echo $?` in
+        0)
+            echo "--- Poetry is already added to the path in ${HOME}/$1."
             ;;
         1)
-            echo "--- Poetry is already added to the path."
+            echo "--- Adding poetry to the path in ${HOME}/$1."
+            echo 'export PATH=${HOME}/.poetry/bin:${PATH}' >> ~/$1
+            source ${HOME}/$1
             ;;
-        *) 
+        *)
             echo "--- You don't seem to have $1. No worries, all is fine :)"
             ;;
     esac
 }
 
 echo "--- Installing Poetry..."
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+#curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 
 update_sh .bashrc
 update_sh .profile
