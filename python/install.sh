e8c7ee688041e5a5c2148a182af00f7492e6f510
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2020-09-07 11:22:06 +0200

    Updates python install script.

--- python/install.sh
+++ python/install.sh
@@ -2,18 +2,18 @@
 
 set -e
 function update_sh {
-    case `grep -Fxq "PYENV_ROOT" ${HOME}/$1 > /dev/null; echo $?` in
-        0) 
+    case `grep -sqF "PYENV_ROOT" ${HOME}/$1 > /dev/null; echo $?` in
+        0)
+            echo "--- pyenv already added to ~/$1."
+            ;;
+        1)
             echo "--- Adding pyenv environment variables to ~/$1."
             echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/$1
             echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/$1
             echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/$1
             source ${HOME}/$1
             ;;
-        1) 
-            echo "--- pyenv already added to ~/$1."
-            ;;
-        *) 
+        *)
             echo "--- You don't seem to have $1. No worries, all is fine :)"
             ;;
     esac
@@ -31,9 +31,9 @@ fi
 cd ${CURRENT_DIR}
 
 update_sh .bashrc
+update_sh .profile
 update_sh .bash_profile
 update_sh .zshrc
-update_sh .profile
 
 if [[ `uname -s` == "Darwin" ]]; then
     ${HOME}/.dotfiles/python/build_python_macos.sh 3.6.8
