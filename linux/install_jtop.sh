4bea647203a39f75833bb24fb2241191c1fc7363
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2021-09-02 14:12:55 +0200

    Improvements

--- linux/install_jtop.sh
+++ linux/install_jtop.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt install python3-pip
 
 sudo -H /usr/bin/pip3 install jetson-stats
-sudo sytemctl restart jetson_stats.service
+sudo /bin/systemctl restart jetson_stats.service
