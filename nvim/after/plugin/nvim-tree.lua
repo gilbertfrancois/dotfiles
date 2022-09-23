b3c37ea69e7278ad57988516c2e492f4a8801053
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2022-09-23 13:31:20 +0200

    Removed deprecated setting

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -27,7 +27,6 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   view = {
     adaptive_size = false,
     width = 30,
-    height = 30,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
