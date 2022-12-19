2c0b995b6da8469e7692d6321fed14270ab29a56
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2022-12-19 12:54:24 +0100

    New key mappings for telescope.

--- nvim/init.lua
+++ nvim/init.lua
@@ -149,21 +149,23 @@ require('telescope').setup {
 -- Enable telescope fzf native
 require('telescope').load_extension 'fzf'
 
---Add leader shortcuts
-vim.keymap.set('n', ';f', function()
+
+-- Find ...
+vim.keymap.set('n', '<leader>ff', function()
     require('telescope.builtin').find_files { previewer = false }
 end)
-vim.keymap.set('n', ';g', require('telescope.builtin').git_files)
-vim.keymap.set('n', ';r', require('telescope.builtin').live_grep)
-vim.keymap.set('n', ';d', require('telescope.builtin').diagnostics)
-vim.keymap.set('n', '\\\\', require('telescope.builtin').buffers)
-vim.keymap.set('n', ';;', require('telescope.builtin').help_tags)
--- vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
--- vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
--- vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
--- vim.keymap.set('n', '<leader>so', function()
---   require('telescope.builtin').tags { only_current_buffer = true }
--- end)
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files)
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics)
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
+-- Search ...
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
+vim.keymap.set('n', '<leader>so', function()
+    require('telescope.builtin').tags { only_current_buffer = true }
+end)
 -- vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
 
 -- Treesitter configuration
