ca5a08485330026d9299c09981a9065464c1012d
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-01-24 13:27:20 +0100

    Updates macOS package install script.

--- macos/install_packages.sh
+++ macos/install_packages.sh
@@ -5,12 +5,12 @@ sudo xcodebuild -runFirstLaunch
 
 echo "--- Installing packages..."
 brew update
-brew install mosh tmux htop bear clang-format llvm
+brew install mosh tmux htop bear clang-format llvm oath-toolkit
 
 echo "--- Setting up skuld"
 sudo git config --system --unset credential.helper
-brew tap DEEP-IMPACT-AG/hyperdrive
-brew install skuld
+# brew tap DEEP-IMPACT-AG/hyperdrive
+# brew install skuld
 brew tap homebrew/cask-fonts
 brew install font-iosevka-nerd-font
 brew install font-terminess-ttf-nerd-font
