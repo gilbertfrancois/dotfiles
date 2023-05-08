3b5b24947c75bcaaade518084bb219facf78d9b0
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-05-08 12:55:28 +0200

    Updates nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -486,7 +486,7 @@ require('onedark').setup {
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
     -- toggle theme style ---
-    -- toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+    toggle_style_key = '<leader>cs', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
     toggle_style_list = {'warmer', 'light'}, -- List of styles to toggle between
 
     -- Change code style ---
