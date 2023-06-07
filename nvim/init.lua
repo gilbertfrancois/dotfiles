8ce719a868455377b4e54da2c92da1394bc6b434
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-06-07 16:44:21 +0200

    Updates nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -60,7 +60,7 @@ require('packer').startup(function(use)
     use 'navarasu/onedark.nvim' -- Theme inspired by Atom
     use 'nvim-lualine/lualine.nvim' -- Fancier statusline
     use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-    use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+    -- use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
     use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
     -- Fuzzy Finder (files, lsp, etc)
     use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
@@ -74,6 +74,8 @@ require('packer').startup(function(use)
     -- use 'rcarriga/cmp-dap'
     use 'theHamsta/nvim-dap-virtual-text'
     use 'mfussenegger/nvim-dap-python'
+    -- Latex
+    use 'lervag/vimtex'
 
     -- CoPilot
     use 'github/copilot.vim'
@@ -195,7 +197,7 @@ require('lualine').setup {
 }
 
 -- Enable Comment.nvim
-require('Comment').setup()
+-- require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
