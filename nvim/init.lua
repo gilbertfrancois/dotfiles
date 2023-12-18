bb0f9d4605fdf4581e90ac7b87a0c383a8f807c9
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-12-18 15:02:25 +0100

    modifies lualine and light background

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,6 +147,8 @@ vim.wo.signcolumn = 'yes'
 -- Set colorscheme
 vim.o.termguicolors = true
 vim.cmd [[colorscheme xcodelighthc]]
+-- vim.cmd [[colorscheme default]]
+vim.cmd [[set background=light]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -189,12 +191,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Set lualine as statusline
 -- See `:help lualine.txt`
 require('lualine').setup {
-    options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-    },
+    -- options = {
+    --     icons_enabled = false,
+    --     component_separators = '|',
+    --     section_separators = '',
+    -- },
 }
 
 -- Enable Comment.nvim
