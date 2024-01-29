f03a01c447842b26f1133e703e94dc837491656b
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2024-01-29 11:13:38 +0100

    Switching from light to dark mode

--- nvim/config/nvim/lua/config/options.lua
+++ nvim/config/nvim/lua/config/options.lua
@@ -6,6 +6,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.relativenumber = false
 vim.opt.wrap = true
 vim.opt.linebreak = true
--- nvim/config/nvim/lua/plugins/colorschemes.lua
+++ nvim/config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,20 @@
 return {
   { "projekt0n/github-nvim-theme" }, -- Theme inspired by Github
-
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "warmer",
+      lualine = {
+        transparent = true, -- lualine center bar transparency
+      },
+      transparent = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_light_default",
+      -- colorscheme = "github_light_default",
+      colorscheme = "onedark",
     },
   },
 }
