1a928ac4a8e7fe2af0fc80c4a510b01ae49f3e3e
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2026-04-09 17:01:47 +0200

    Updates...

---
+++ linux/install_clipboard_sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
+gnome-extensions enable clipboard-indicator@tudmotu.com
