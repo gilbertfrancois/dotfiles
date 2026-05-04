2b75712c431d3efca8d99f7bd8ecabbd34204435
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2026-05-04 23:51:00 +0200

    WIP

--- linux/fedora_tweaks.sh
+++ linux/fedora_tweaks.sh
@@ -1,71 +1,148 @@
 #!/bin/bash
 
-# 1. Enable RPM Fusion (Free and Non-Free)
-# This is mandatory for NVIDIA drivers and non-free codecs
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+function pre_install() {
+    sudo dnf install -y \
+        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf makecache --refresh
+}
 
-# 2. Update package metadata
-sudo dnf makecache --refresh
+function install_misc() {
+    sudo dnf update
+    sudo dnf install -y \
+        awscli2 \
+        fastfetch \
+        foot \
+        gnome-extensions-app \
+        gnome-shell-extension-appindicator \
+        htop \
+        libappindicator-gtk3 \
+        lm_sensors \
+        nvtop \
+        oathtool \
+        openssh \
+        texlive-scheme-medium
+    sudo dnf copr enable lihaohong/yazi
+    sudo dnf install -y yazi
+    sudo dnf copr enable dejan/lazygit
+    sudo dnf install -y lazygit
+}
 
-sudo dnf update
-sudo dnf install fastfetch
-sudo dnf install lm_sensors
-sudo dnf install oathtool
-sudo dnf install htop
-sudo dnf install libappindicator-gtk3 gnome-shell-extension-appindicator gnome-extensions-app
-sudo dnf install openssh
+function make_settings() {
+    gsettings set org.gnome.desktop.input-sources xkb-options "['compose:ralt', 'ctrl:nocaps']"
+    sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/10-sub-pixel-rgb.conf
+    $HOME/.dotfiles/linux/install_clipboard_sync.sh
+    sudo cp "$(dirname "$0")/etc_bluetooth_main.conf" /etc/bluetooth/main.conf
+}
 
-sudo dnf install zathura zathura-plugins-all
-sudo dnf install awscli2
-sudo dnf install texlive-scheme-medium
+function install_ffmpeg() {
+    sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+}
 
-gsettings set org.gnome.desktop.input-sources xkb-options "['compose:ralt', 'ctrl:nocaps']"
+function install_vaapi_amd_intel() {
+    sudo dnf remove -y \
+        libva-intel-driver \
+        libva-intel-hybrid-driver \
+        mesa-va-drivers \
+        mesa-vdpau-drivers
+    sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+    sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+    sudo dnf install -y \
+        intel-media-driver \
+        libva-utils \
+        vdpauinfo
+}
 
-sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/10-sub-pixel-rgb.conf
+function install_vaapi_nvidia_intel() {
+    sudo dnf remove -y \
+        libva-intel-driver \
+        libva-intel-hybrid-driver \
+        mesa-va-drivers \
+        mesa-vdpau-drivers
+    sudo dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
+    sudo dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+    sudo dnf install -y \
+        intel-media-driver \
+        libva-utils \
+        vdpauinfo \
+        nvidia-vaapi-driver \
+        libva-nvidia-driver \
+        nv-codec-headers
+}
 
-$HOME/.dotfiles/linux/install_clipboard_sync.sh
+function install_gstreamer() {
+    sudo dnf install -y \
+        gstreamer1 \
+        gstreamer1-devel \
+        gstreamer1-plugins-base \
+        gstreamer1-plugins-base-devel \
+        gstreamer1-plugins-base-tools \
+        gstreamer1-plugins-good \
+        gstreamer1-plugins-good-extras \
+        gstreamer1-plugins-bad-free \
+        gstreamer1-plugins-bad-free-devel \
+        gstreamer1-plugins-bad-free-extras \
+        gstreamer1-plugins-bad-freeworld \
+        gstreamer1-plugins-ugly \
+        gstreamer1-plugins-ugly-free \
+        gstreamer1-plugins-ugly-free-extras \
+        gstreamer1-plugin-libav \
+        gstreamer1-plugin-openh264 \
+        gstreamer1-plugins-entrans \
+        gstreamer1-vaapi \
+        gstreamer1-libcamera
+    sudo dnf group upgrade -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+    rm -rf ~/.cache/gstreamer-1.0
+}
 
-# 3. Swap ffmpeg-free for the full-featured ffmpeg
-# This replaces the limited Fedora version with the one supporting NVENC/NVDEC and QuickSync
-sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+function install_docker() {
+    sudo dnf remove docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-selinux \
+        docker-engine-selinux \
+        docker-engine
+    sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
 
-# 4. Install Intel & NVIDIA Hardware Acceleration Drivers (VA-API/VDPAU)
-# intel-media-driver is the optimized non-free driver for 8th+ gen Intel CPUs
-# nvidia-vaapi-driver allows NVIDIA to work with VA-API applications
-sudo dnf install -y \
-    intel-media-driver \
-    libva-utils \
-    vdpauinfo \
-    nvidia-vaapi-driver \
-    libva-nvidia-driver
+function install_nvidia_container_toolkit() {
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+    export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.19.0-1
+    sudo dnf install -y \
+        nvidia-container-toolkit-${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+        nvidia-container-toolkit-base-${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+        libnvidia-container-tools-${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+        libnvidia-container1-${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+}
 
-# 5. Install the complete GStreamer Suite
-# We include 'freeworld' and 'ugly' versions which contain the optimized/proprietary codecs
-sudo dnf install -y \
-    gstreamer1 \
-    gstreamer1-plugins-base \
-    gstreamer1-plugins-base-tools \
-    gstreamer1-plugins-good \
-    gstreamer1-plugins-good-extras \
-    gstreamer1-plugins-bad-free \
-    gstreamer1-plugins-bad-free-extras \
-    gstreamer1-plugins-bad-freeworld \
-    gstreamer1-plugins-ugly \
-    gstreamer1-plugins-ugly-free \
-    gstreamer1-plugin-libav \
-    gstreamer1-vaapi \
-    gstreamer1-libcamera
+function install_tex() {
+    sudo dnf install -y \
+        texlive-scheme-tetex \
+        texlive-tabularray \
+        zathura \
+        zathura-plugins-all
+}
 
-# 6. Install Multimedia Group (Standardized Metadata and Codecs)
-# This ensures system-wide support for all container types
-sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+function install_generic() {
+    pre_install
+    make_settings
+    install_misc
+    install_docker
+    install_ffmpeg
+    install_gstreamer
+    install_tex
+}
 
-# 7. Development Headers (Only if you are compiling software)
-sudo dnf install -y gstreamer1-devel gstreamer1-plugins-base-devel
+function install_x86_amd() {
+    install_vaapi_amd_intel
+}
 
-dnf copr enable lihaohong/yazi
-dnf install yazi
-
-sudo dnf install texlive-scheme-tetex
-sudo dnf install texlive-tabularray
+function install_x86_nvidia() {
+    install_vaapi_nvidia_intel
+    install_nvidia_container_toolkit
+}
