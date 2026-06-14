1b65308b6364564ae293e7331089257992e8fafd
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2026-06-15 00:00:51 +0200

    Adds macbook install settings

---
+++ linux/macbook-pro.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "$(dirname "$0")/fedora_tweaks.sh"
+
+# function install_dell_settings() {
+    # sudo grubby --update-kernel=ALL --remove-args="acpi_backlight=vendor"
+    # sudo grubby --update-kernel=ALL --args="i915.enable_lspcon=0"
+# }
+# 
+# function install_nvidia_580xx() {
+    # sudo dnf remove "*nvidia*" -x nvidia-gpu-firmware
+    # sudo dnf install -y \
+        # akmod-nvidia-580xx \
+        # xorg-x11-drv-nvidia-580xx-cuda
+# }
+
+install_generic
+# install_dell_settings
+# install_nvidia_580xx
+# install_x86_nvidia
