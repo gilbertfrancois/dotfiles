1b26528c6d251de8a5b4fbfceb2661c837430a2d
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2026-07-05 17:37:14 +0200

    Cleaning up

--- hyprland/rofi/scripts/rofi-window.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec env ROFI_WINDOW=true rofi -show window -modes window -theme "$HOME/.config/rofi/config.rasi"
--- hyprland/waybar/scripts/workspace-click.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-hyprctl dispatch "hl.dsp.focus({ workspace = $1 })"
