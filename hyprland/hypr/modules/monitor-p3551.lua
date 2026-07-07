ddf51b74ec5309e7948fd6aad71e66c52105b90b
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2026-07-08 00:16:12 +0200

    Updates monitor settings

--- hyprland/hypr/modules/monitor-p3551.lua
+++ hyprland/hypr/modules/monitor-p3551.lua
@@ -1,25 +1,25 @@
 hl.monitor({
 	output = "desc:Apple Computer Inc iMac 58C231771DD27",
-	mode = "3840x2160@60.0",
+	mode = "3840x2160@60",
 	position = "0x0",
 	scale = 1.5,
 })
 hl.monitor({
-	output = "desc:AU Optronics 0x26ED Unknown",
-	mode = "1920x1080@60.06",
+	output = "desc:AU Optronics 0x26ED",
+	mode = "1920x1080@60",
 	position = "0x0",
 	scale = 1,
 })
 hl.monitor({
 	output = "desc:Seiko Epson Corporation EPSON PJ",
-	mode = "1920x1080@60.00000",
+	mode = "1920x1080@60",
 	position = "0x0",
 	scale = 1,
 	mirror = "eDP-1",
 })
 hl.monitor({
 	output = "desc:Dell Inc. DELL P2415Q D8VXF994054B",
-	mode = "3840x2160@59.99600",
+	mode = "3840x2160@60",
 	position = "-2560x0",
 	scale = 1.5,
 })
