7d26aaff39a33bb56e083245056abb44e03bdcf3
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2026-07-07 20:33:33 +0200

    Cleanup of scripts

--- linux/machine_specific/dell-p3551.sh
+++ linux/machine_specific/dell-p3551.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-source "$(dirname "$0")/fedora_tweaks.sh"
-
 function install_dell_settings() {
     sudo grubby --update-kernel=ALL --args="acpi_osi=! acpi_osi='Windows 2015' acpi_backlight=native mem_sleep_default=deep"
     sudo grubby --update-kernel=ALL --args="rd.driver.blacklist=nouveau,nova_core modprobe.blacklist=nouveau,nova_core"
-}
-
-function install_logind_settings() {
-    sudo mkdir -p /etc/systemd/logind.conf.d
-    sudo tee /etc/systemd/logind.conf.d/power.conf > /dev/null << 'EOF'
-[Login]
-HandlePowerKey=suspend
-EOF
-    sudo systemctl restart systemd-logind
 }
 
 function install_nvidia_580xx() {
@@ -23,8 +12,5 @@ function install_nvidia_580xx() {
         xorg-x11-drv-nvidia-580xx-cuda
 }
 
-install_generic
 install_dell_settings
-install_logind_settings
 install_nvidia_580xx
-install_x86_nvidia
