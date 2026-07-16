ac474623d530373cd40d4b53120930112029c680
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2026-07-16 23:35:11 +0200

    Adds macbook air monitor

---
+++ hyprland/hypr/modules/monitor-macbookair.lua
@@ -0,0 +1,12 @@
+hl.monitor({
+	output = "eDP-1",
+	mode = "2560x1600@60.00000",
+	position = "0x0",
+	scale = 1.6,
+})
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+})
