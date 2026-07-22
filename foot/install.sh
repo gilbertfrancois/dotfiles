a7f8b76c17272fd0d9d9053153516c1273320a84
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2026-07-22 15:40:12 +0200

    Updates foot

--- foot/install.sh
+++ foot/install.sh
@@ -1,37 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-PREFIX="$HOME/.local/foot.app"
-
-# sudo apt update
-# sudo apt install -y \
-    # build-essential git pkg-config meson ninja-build scdoc python3 \
-    # libxkbcommon-dev libwayland-dev wayland-protocols \
-    # libpixman-1-dev libfontconfig1-dev \
-    # libutf8proc-dev libncursesw5-dev \
-    # libfcft-dev libtllist-dev
-
-sudo dnf install -y \
-    @development-tools git pkgconf-pkg-config meson ninja-build scdoc python3 \
-    libxkbcommon-devel wayland-devel wayland-protocols-devel \
-    pixman-devel fontconfig-devel \
-    utf8proc-devel ncurses-devel \
-    fcft-devel tllist-devel wayland-utils
-
-cd /tmp
-rm -rf foot
-git clone https://codeberg.org/dnkl/foot.git
-cd foot
-
-mkdir -p ~/.config/systemd/user
-
-export CFLAGS="${CFLAGS:-} -O3"
-
-meson setup bld/release \
-    --buildtype=release \
-    --prefix="$HOME/.local/foot.app" \
-    -Db_lto=true \
-    -Dsystemd-units-dir="$HOME/.local/share/systemd/user"
-ninja -C bld/release
-ninja -C bld/release test
-ninja -C bld/release install
+cp foot-launch.sh $HOME/.local/bin
+cp foot.desktop $HOME/.local/share/applications/foot.desktop
+ln -s $HOME/.dotfiles/foot $HOME/.config
